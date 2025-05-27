e1ca0a2876661e2c4abdc462e74b7b90742fb540
nandomattos <luizfernandoamattos@gmail.com>
2025-05-27 01:15:12 -0300

    FAEAT: armazenar histórico de alimentos do usuário

--- lib/repository/alimento_repository.dart
+++ lib/repository/alimento_repository.dart
@@ -1,8 +1,56 @@
-import 'package:flutter/cupertino.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import '../database/db_firestore.dart';
+import '../model/alimento.dart';
+import '../service/auth_service.dart';
 
-class AlimentoRepository extends ChangeNotifier {
-  List <RegistroAlimento> _alimentoList = [];
+class AlimentoRepository {
+  final FirebaseFirestore _firestore = DBFirestore.get();
+  final AuthService _authService;
+
+  AlimentoRepository(this._authService);
+
+  CollectionReference<Map<String, dynamic>> _alimentoRef() {
+    final user = _authService.usuario;
+    if (user == null) {
+      throw Exception('Usuário não autenticado');
+    }
+    return _firestore.collection('usuarios').doc(user.uid).collection('alimentos');
+  }
+
+  Future<void> adicionar(Alimento alimento) async {
+    await _alimentoRef().add({
+      'nome': alimento.nome,
+      'caloriasPorUnidade': alimento.caloriasPorUnidade,
+      'quantidade': alimento.quantidade,
+      'dataInsercao': alimento.dataInsercao.toIso8601String(),
+    });
+  }
+
+  Future<void> remover(String alimentoId) async {
+    await _alimentoRef().doc(alimentoId).delete();
+  }
+
+  Stream<List<AlimentoFirestore>> getAlimentosStream() {
+    return _alimentoRef().orderBy('dataInsercao', descending: true).snapshots().map(
+          (snapshot) => snapshot.docs.map((doc) {
+        final data = doc.data();
+        return AlimentoFirestore(
+          id: doc.id,
+          alimento: Alimento(
+            nome: data['nome'],
+            caloriasPorUnidade: (data['caloriasPorUnidade'] as num).toDouble(),
+            quantidade: (data['quantidade'] as num).toInt(),
+            dataInsercao: DateTime.parse(data['dataInsercao']),
+          ),
+        );
+      }).toList(),
+    );
+  }
 }
 
-class RegistroAlimento {
+class AlimentoFirestore {
+  final String id;
+  final Alimento alimento;
+
+  AlimentoFirestore({required this.id, required this.alimento});
 }
